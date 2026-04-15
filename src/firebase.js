fbdbd3afc684adf439ea38041aa86aa8ce60df99
nasimsiddique <nasimsiddique71@gmail.com>
2026-04-15 20:56:00 +0530

    Added Vercel routing configuration

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"; // ✅ ADD THIS
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_KEY",
+  apiKey: "AIzaSyAaYrD2NlgY185CHMmxe62UQ0AS3US0BnY",
   authDomain: "sbtply-admin.firebaseapp.com",
   projectId: "sbtply-admin",
   storageBucket: "sbtply-admin.firebasestorage.app",
   messagingSenderId: "670178226895",
-  appId: "1:670178226895:web:xxxx"
+  appId: "1:670178226895:web:4e894911b77d64d285bdba"
 };
 
 const app = initializeApp(firebaseConfig);
